c0423f3a00c0b661bdc908db20230f7f8b7d9740
Xiegrain Akasaki <mail@xiegrain-akasaki.github.com>
2020-07-05 11:05:26 +0700

    mobile navbar collapsed when link clicked

--- js/handler.js
+++ js/handler.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('.hamburger-menu-button').on('click', function() {
     $('.navbar').toggleClass('navbar-active')
   });
+
+  $('.menu-link').on('click', function() {
+    $('.navbar').toggleClass('navbar-active')
+  })
 });
