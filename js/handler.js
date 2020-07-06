d907a327c15b7d8fce0fdf4634284f8d462153d5
Xiegrain Akasaki <48846347+XiegrainAkasaki@users.noreply.github.com>
2020-07-06 23:31:02 +0700

    Adjustment

--- js/handler.js
+++ js/handler.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-  $('.container-background').ready(function() {
-    $('link').ready(function() {
-      $('.share').addClass('share-pop');
-    });
-  });
+  $('.share').addClass('share-pop');
 
   $('.hamburger-menu-button').on('click', function() {
     $('.navbar').toggleClass('navbar-active');
