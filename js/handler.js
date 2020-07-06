3a18892b4794752785c7160aedfa98daef90a8e3
Xiegrain Akasaki <mail@xiegrain-akasaki.github.com>
2020-07-06 17:11:46 +0700

    share animation delayed until all loaded

--- js/handler.js
+++ js/handler.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
+  $('.container-background').ready(function() {
+    $('link').ready(function() {
+      $('.share').addClass('share-pop');
+    });
+  });
+
   $('.hamburger-menu-button').on('click', function() {
-    $('.navbar').toggleClass('navbar-active')
+    $('.navbar').toggleClass('navbar-active');
   });
 
   $('.menu-link').on('click', function() {
-    $('.navbar').toggleClass('navbar-active')
-  })
+    $('.navbar').toggleClass('navbar-active');
+  });
 });
