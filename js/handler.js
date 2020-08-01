fd382e3bd65b908e3430f5ea0b8dd8243dfa4901
Xiegrain Akasaki <mail@xiegrain-akasaki.github.com>
2020-08-01 08:55:19 +0700

    dynamic content on development

--- js/handler.js
+++ js/handler.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
-  $('.share').addClass('share-pop');
+  $('.share').delay(500).addClass('share-pop');
 
   $('.burger-button').on('click', function() {
     $('.navbar').toggleClass('navbar-active');
   });
 
   $('.menu-link').on('click', function() {
-    $('.navbar').toggleClass('navbar-active');
+    $('.navbar').removeClass('navbar-active');
+  });
+
+  $('.about, .showcase, .contact, .support').click(function() {
+    $('.title').css({'transform' : 'scale(' + 0 + ')'});
+    $('.dynamic-content').animate({height: '100%'});
+  });
+
+  $('.home').click(function() {
+    $('.dynamic-content').animate({height: '0'});
+    $('.title').css({'transform' : 'scale(' + 1 + ')'});
+  });
+});
+
+$('.about, .showcase, .contact, .support').on('click', function() {
+  $('.title').removeClass('title-active').delay(500).queue(function() {
+    $('.dynamic-content').addClass('content-active').dequeue();
+  });
+});
+
+$('.home').on('click', function() {
+  $('.dynamic-content').removeClass('content-active').delay(500).queue(function() {
+    $('.title').addClass('title-active').dequeue();
   });
 });
